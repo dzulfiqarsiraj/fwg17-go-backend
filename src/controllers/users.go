36fb2581b25038c2f20fc8688b9c046a3c747ad0
Dzulfiqar <103058254+DzulfiqarSiraj@users.noreply.github.com>
2024-01-30 22:07:42 +0700

    add create/update/delete controller

--- src/controllers/users.go
+++ src/controllers/users.go
@@ -23,10 +23,15 @@ type response struct {
 	Results interface{} `json:"results"`
 }
 
+type responseOnly struct {
+	Success bool   `json:"success"`
+	Message string `json:"message"`
+}
+
 type User struct {
-	Id       int    `json:"id"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	Id       int    `json:"id" form:"id"`
+	Email    string `json:"email" form:"email"`
+	Password string `json:"password" form:"password"`
 }
 
 func ListAllUsers(c *gin.Context) {
@@ -60,6 +65,49 @@ func DetailUser(c *gin.Context) {
 		Results: User{
 			Id:       id,
 			Email:    "admin@mail.com",
+			Password: "1234",
+		},
+	})
+}
+
+func CreateUser(c *gin.Context) {
+	user := User{}
+	c.Bind(&user)
+	c.JSON(http.StatusOK, &response{
+		Success: true,
+		Message: "User Created Successfully",
+		Results: user,
+	})
+}
+
+func UpdateUser(c *gin.Context) {
+	id, _ := strconv.Atoi(c.Param("id"))
+	// if err != nil {
+	// 	c.JSON(http.StatusBadRequest, &responseOnly{
+	// 		Success: false,
+	// 		Message: "Required Param id",
+	// 	})
+	// }
+	user := User{}
+
+	c.Bind(&user)
+	user.Id = id
+	c.JSON(http.StatusOK, &response{
+		Success: true,
+		Message: "Update User Successfully",
+		Results: user,
+	})
+}
+
+func DeleteUser(c *gin.Context) {
+	id, _ := strconv.Atoi(c.Param("id"))
+
+	c.JSON(http.StatusOK, &response{
+		Success: true,
+		Message: "User Deleted Successfully",
+		Results: User{
+			Id:       id,
+			Email:    "guest@mail.com",
 			Password: "1234",
 		},
 	})
