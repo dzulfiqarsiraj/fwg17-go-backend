876d52ba686ef233ba2447918d1c20aab5132586
Dzulfiqar <103058254+DzulfiqarSiraj@users.noreply.github.com>
2024-02-17 17:53:59 +0700

    feat: add get products for public endpoint

---
+++ src/controllers/products.go
@@ -0,0 +1,73 @@
+package controllers
+
+import (
+	"log"
+	"math"
+	"net/http"
+	"strconv"
+	"strings"
+
+	"github.com/DzulfiqarSiraj/go-backend/src/models"
+	"github.com/DzulfiqarSiraj/go-backend/src/services"
+	"github.com/gin-gonic/gin"
+)
+
+func ListAllProducts(c *gin.Context) {
+	page, _ := strconv.Atoi(c.DefaultQuery("page", "1"))
+	limit, _ := strconv.Atoi(c.DefaultQuery("limit", "6"))
+	search := c.DefaultQuery("search", "")
+	orderBy := c.DefaultQuery("orderBy", "id")
+
+	offset := (page - 1) * limit
+	result, err := models.FindAllProducts(search, orderBy, limit, offset)
+
+	pageInfo := &services.PageInfo{
+		Page:      page,
+		Limit:     limit,
+		TotalPage: int(math.Ceil(float64(result.Count) / float64(limit))),
+		TotalData: result.Count,
+	}
+
+	if err != nil {
+		log.Fatalln(err)
+		c.JSON(http.StatusInternalServerError, &services.ResponseOnly{
+			Success: false,
+			Message: "Internal Server Error",
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, &services.ResponseList{
+		Success:  true,
+		Message:  "List All Products",
+		PageInfo: *pageInfo,
+		Results:  result.Data,
+	})
+}
+
+func DetailProduct(c *gin.Context) {
+	id, _ := strconv.Atoi(c.Param("id"))
+
+	product, err := models.FindOneProduct(id)
+	if err != nil {
+		log.Println(err)
+		if strings.HasPrefix(err.Error(), "sql: no rows") {
+			c.JSON(http.StatusNotFound, &services.ResponseOnly{
+				Success: false,
+				Message: "Product Not Found",
+			})
+			return
+		}
+		c.JSON(http.StatusInternalServerError, &services.ResponseOnly{
+			Success: false,
+			Message: "Internal Server Error",
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, &services.Response{
+		Success: true,
+		Message: "Detail Product",
+		Results: product,
+	})
+}
--- src/routers/combine.go
+++ src/routers/combine.go
@@ -2,10 +2,13 @@ package routers
 
 import (
 	"github.com/DzulfiqarSiraj/go-backend/src/routers/admin"
+	"github.com/DzulfiqarSiraj/go-backend/src/routers/customer"
 	"github.com/gin-gonic/gin"
 )
 
 func Combine(r *gin.Engine) {
+	ProductRouter(r.Group("/products"))
 	AuthRouter(r.Group("/auth"))
 	admin.Combine(r.Group("/admin"))
+	customer.Combine(r.Group("/customer"))
 }
