28565aa72d69709df0b0ffdf5a8d57d0be8975c0
Dzulfiqar <103058254+DzulfiqarSiraj@users.noreply.github.com>
2024-05-26 23:14:21 +0700

    feat: add uploading file upload src to cloudinary and delete existing file at cloudinary

--- src/controllers/customer/profile.go
+++ src/controllers/customer/profile.go
@@ -1,16 +1,18 @@
 package customer_controllers
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
-	"os"
 	"strings"
 
 	"github.com/DzulfiqarSiraj/go-backend/src/lib"
 	"github.com/DzulfiqarSiraj/go-backend/src/models"
 	"github.com/DzulfiqarSiraj/go-backend/src/services"
 	"github.com/KEINOS/go-argonize"
+
+	"github.com/cloudinary/cloudinary-go/v2/api/uploader"
 	"github.com/gin-gonic/gin"
 )
 
@@ -46,8 +48,11 @@ func UpdateProfile(c *gin.Context) {
 	customer := c.MustGet("id").(*models.User)
 	id := customer.Id
 
-	fileInput, _, _ := c.Request.FormFile("picture")
-	fmt.Println(&fileInput)
+	data := models.User{}
+
+	c.ShouldBind(&data)
+
+	fileInput, _ := c.FormFile("pictures")
 
 	existingUser, err := models.FindOneUser(id)
 	if err != nil {
@@ -60,10 +65,6 @@ func UpdateProfile(c *gin.Context) {
 		}
 		fmt.Println(existingUser)
 	}
-
-	data := models.User{}
-
-	c.ShouldBind(&data)
 
 	if data.Password != "" {
 		plain := []byte(data.Password)
@@ -79,9 +80,9 @@ func UpdateProfile(c *gin.Context) {
 		data.Password = hash.String()
 	}
 
-	// upload file
+	// upload file -start
 	if fileInput != nil {
-		data.Pictures = lib.Upload(c, "picture", "users")
+		data.Pictures, _ = lib.Upload(c, "pictures", "users")
 		if *data.Pictures == "Invalid File Type" {
 			c.JSON(http.StatusBadRequest, &services.ResponseOnly{
 				Success: false,
@@ -99,13 +100,33 @@ func UpdateProfile(c *gin.Context) {
 		}
 
 		if existingUser.Pictures != nil {
-			fileName := *existingUser.Pictures
-			fileDest := fmt.Sprintf("uploads/users/%v", fileName)
-			fmt.Println(fileDest)
-			os.Remove(fileDest)
+			// Delete src picture from local storage - start
+			// fileName := *existingUser.Pictures
+			// fileDest := fmt.Sprintf("cov-shop/users/%v", fileName)
+			// os.Remove(fileDest)
+			// Delete src picture from local storage - end
+
+			// Delete src picture from cloudinary - start
+			existingPicture := strings.Split(*existingUser.Pictures, "/")
+			slicedExistingPicture := strings.Split(existingPicture[9], ".")
+			existingPublicId := "cov-shop/users/" + slicedExistingPicture[0]
+			fmt.Println(existingPublicId)
+			cloud := lib.CloudinaryConfig()
+			var ctx = context.Background()
+			resp, err := cloud.Upload.Destroy(ctx, uploader.DestroyParams{
+				PublicID: existingPublicId,
+			})
+
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+
+			fmt.Println(resp.Result)
+			// Delete src picture from cloudinary - start
 		}
 	}
-	// *upload file
+	// upload file - end
 
 	data.Id = id
 
